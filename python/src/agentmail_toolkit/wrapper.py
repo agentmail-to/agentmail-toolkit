19d524cc10a8938d5b53d32572fbea35f8a61063
Haakam Aujla <haakamsingh21@gmail.com>
2025-11-29 18:38:11 -0500

    Cleanup

--- python/src/agentmail_toolkit/wrapper.py
+++ python/src/agentmail_toolkit/wrapper.py
@@ -4,12 +4,6 @@ from pydantic import BaseModel
 from agentmail import AgentMail
 
 
-class Attachment(BaseModel):
-    text: Optional[str] = None
-    error: Optional[str] = None
-    file_type: Optional[str] = None
-
-
 class Wrapper(ABC):
     _client: AgentMail = None
 
